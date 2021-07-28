6bfa0b9966d347e8ae94ce5e2d9fbdf2d5f60185
Mark Waterman <markwaterman@users.noreply.github.com>
2021-07-28 15:47:44 -0700

    Break CartItem out into its own source file.

--- ShoppingCart/Cart.cs
+++ ShoppingCart/Cart.cs
@@ -18,24 +18,4 @@ namespace ShoppingCart
         [Key(1)]
         public List<CartItem> Items { get; set; }
     }
-
-
-    /// <summary>
-    /// Shopping cart item DTO.
-    /// </summary>
-    [MessagePackObject]
-    public class CartItem
-    {
-        [Key(0)]
-        public string ProductName { get; set; }
-
-        [Key(1)]
-        public int Quantity { get; set; }
-
-        [Key(2)]
-        public decimal Price { get; set; }
-
-        [Key(3)]
-        public bool Backordered { get; set; }
-    }
 }
---
+++ ShoppingCart/CartItem.cs
@@ -0,0 +1,25 @@
+﻿using MessagePack;
+
+// Required NuGet package: MessagePack.Annotations
+
+namespace ShoppingCart
+{
+    /// <summary>
+    /// Shopping cart item DTO.
+    /// </summary>
+    [MessagePackObject]
+    public class CartItem
+    {
+        [Key(0)]
+        public string ProductName { get; set; }
+
+        [Key(1)]
+        public int Quantity { get; set; }
+
+        [Key(2)]
+        public decimal Price { get; set; }
+
+        [Key(3)]
+        public bool Backordered { get; set; }
+    }
+}
